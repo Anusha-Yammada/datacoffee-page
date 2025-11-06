1750f0b1c2c046fda5d992340f19079d5f8df6da
Anusha-Yammada <anuyammada@gmail.com>
2025-11-06 13:39:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/datacoffee-page/', // MUST match your GitHub repo name!
   server: {
     host: '0.0.0.0',
     port: 5000,
-  }
-})
+  },
+});
